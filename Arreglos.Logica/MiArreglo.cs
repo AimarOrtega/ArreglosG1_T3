4550b7020b2d5c358f310004a8f3de0e9be2f99a
AimarOrtega <25030201@itesa.edu.mx>
2026-09-17 14:49:47 -0600

    Versión_02: Método llenar.

---
+++ Arreglos.Logica/MiArreglo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arreglos.Logica
+{
+    public class MiArreglo
+    {
+        //Atributos o campos
+        private int _tope;
+        private int[] _arreglo;
+        //Constructor
+        public MiArreglo(int n) //recibe tamanio
+        {
+            N = n;
+            _arreglo = new int[N];      //al no darle setter no se puede cambiar el tamaño, esto se debe proteger
+            _tope = 0;
+        }
+
+        //Propiedades
+        public int N { get; }
+        public bool EstaLleno => _tope == N;
+        public bool EstaVacio => _tope == 0;
+        //Metodos
+        public void Llenar(int minimo, int maximo)
+        {
+            Random oRandom = new Random();
+            for(int i = 0; i<N; i++)
+            {
+                _arreglo[i] = oRandom.Next(minimo, maximo);
+            }
+            _tope = N;  //reescalar el tope al tamaño del arreglo
+        }
+
+        //Metodo ToString
+        public override string ToString()
+        {
+            if(EstaVacio)
+            {
+                return "Arreglo vacio";
+            }
+
+
+            string cadena = string.Empty;       //limpiar cadena vacia
+            int contador = 0; 
+            for (int i = 0; i < _tope; i++)
+            {
+                cadena += $"{_arreglo[i]}\t";    //concatenar los elementos del arreglo
+                contador++;
+                if (contador > 9)
+                {
+                    contador = 0;      //reiniciar el contador
+                    cadena += "\n";      //salto de linea cada 10 elementos
+                }
+            }
+            return cadena;
+        }
+    }
+}
---
+++ Arreglos.UI.Consola/Program.cs
@@ -0,0 +1,8 @@
+﻿using Arreglos.Logica;
+
+Console.WriteLine("Operaciones de pila");
+
+MiArreglo oMiArreglo = new MiArreglo(100);   //tamaño fisico
+oMiArreglo.Llenar(1, 20);      //Tamaño logico
+Console.WriteLine(oMiArreglo);
+//Console.WriteLine(oMiArreglo.EstaVacio);
